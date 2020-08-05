75db3c711bf616b780f51402039035be2ba9e0c2
Ryan <yangchuansheng33@gmail.com>
2020-08-05 12:54:05 +0800

    Update index.go

--- config/index.go
+++ config/index.go
@@ -27,10 +27,10 @@ handle_cookie: True
 
 host:
   self: google.ryanyang.now.sh
-  proxy: www.google.com
+  proxy: gcr.io
 
 replaced_urls:
-  - old: www.google.com
+  - old: gcr.io
     new: google.ryanyang.now.sh
 `
 var Config *Yaml
